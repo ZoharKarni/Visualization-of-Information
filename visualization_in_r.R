d22f202e5ebddf8f19e06e375b06d5e8ae865e9c
ZoharKarni <karniz@post.bgu.ac.il>
2022-01-20 13:55:28 +0200

    added files for first time

--- visualization_in_r.R
+++ visualization_in_r.R
@@ -39,21 +39,3 @@ ggplot(dataset,
   labs(title="Distribution of Temperature by Vacation Type", x="Temperature (Celsius)", y="Vacation Type")+
   theme(plot.title = element_text(size=18, hjust = 0.5),axis.title.x=element_text(size=10,hjust = 0.5),axis.title.y=element_text(size=10,hjust = 0.5),axis.text.y=element_text(size=9))+
   scale_fill_brewer(palette="Accent")
-
-# romove #
-ggplot(dataset, 
-       aes(x = temperature, 
-           fill = PropertyStyle)) +
-  geom_density(alpha = 0.4) +
-  labs(title="A", x="x", y="s")+
-  theme(plot.title = element_text(size=15, hjust = 0.5),axis.title.x=element_text(size=10,hjust = 0.5),axis.title.y=element_text(size=10,hjust = 0.5))
-
-
-ggplot(subset2, aes(x=WW.Erea, y=Profit, fill=Gender)) + 
-  geom_boxplot()+
-  labs(title="Profit by World Area and Gender", x="World Area", y="Profit ($)")+
-  theme(plot.title = element_text(size=18, hjust = 0.5),axis.title.x=element_text(size=10,hjust = 0.5),axis.title.y=element_text(size=10,hjust = 0.5),axis.text.x = element_text(angle = 15))+
-  scale_fill_brewer(palette="Pastel1",labels = c("Female", "Male", "Other"))
-
-ggplot(dataset, aes(x=Price.per.traveler, y=Weeks.before.vacatuin.starts)) + geom_point()
-# romove #
